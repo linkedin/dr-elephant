9c2acc4493f9a6eb39a29e4910130550ad54ad6f
cjuexuan <cjuexuan@gmail.com>
2016-04-27 18:44:49 +0800

    fix exlude error
    change hadoop version : 2.3.0,spark version 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,12 +31,12 @@ object Dependencies {
   lazy val HADOOP_VERSION = "hadoopversion"
   lazy val SPARK_VERSION = "sparkversion"
 
-  var hadoopVersion = "2.7.2"
+  var hadoopVersion = "2.3.0"
   if (System.getProperties.getProperty(HADOOP_VERSION) != null) {
     hadoopVersion = System.getProperties.getProperty(HADOOP_VERSION)
   }
 
-  var sparkVersion = "1.6.1"
+  var sparkVersion = "1.4.0"
   if (System.getProperties.getProperty(SPARK_VERSION) != null) {
     sparkVersion = System.getProperties.getProperty(SPARK_VERSION)
   }
