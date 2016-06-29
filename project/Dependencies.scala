7690bc4fb35e32000aff187ec02e467434562e0e
Arpan Agrawal <aragrawal@linkedin.com>
2016-06-29 02:43:46 -0700

    Reverted dependency changes

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,11 +20,11 @@ import sbt._
 object Dependencies {
 
   // Dependency Version
-  lazy val commonsCodecVersion = "1.9" //default was 1.10,changed it to 1.9 for checking azkaban client
+  lazy val commonsCodecVersion = "1.10"
   lazy val commonsIoVersion = "2.4"
   lazy val gsonVersion = "2.2.4"
   lazy val guavaVersion = "18.0"          // Hadoop defaultly are using guava 11.0, might raise NoSuchMethodException
-  lazy val jacksonMapperAslVersion = "1.9.13" //default was 1.7.3, chaing for trying azkaban client
+  lazy val jacksonMapperAslVersion = "1.7.3"
   lazy val jsoupVersion = "1.7.3"
   lazy val mysqlConnectorVersion = "5.1.36"
   lazy val httpMimeVersion = "4.5"
@@ -72,6 +72,7 @@ object Dependencies {
     "org.codehaus.jackson" % "jackson-mapper-asl" % jacksonMapperAslVersion,
     "org.jsoup" % "jsoup" % jsoupVersion,
     "org.mockito" % "mockito-core" % "1.10.19",
+    "org.jmockit" % "jmockit" % "1.23" % Test,
     "org.apache.httpcomponents" % "httpmime" % httpMimeVersion,
     "org.json" % "json" % "20090211"
   ) :+ sparkExclusion
