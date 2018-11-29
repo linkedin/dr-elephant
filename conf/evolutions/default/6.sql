0fe0800aa052afe1bf3066bac4c59fc885185b8b
ShubhamGupta29 <IIT2014061@iiita.ac.in>
2018-11-29 12:25:09 +0530

    Update 6.sql for removing NOT NULL condition
    Removing NOT NULL condition for show_recommendation_count column since a default_value of 0 is already set.

--- conf/evolutions/default/6.sql
+++ conf/evolutions/default/6.sql
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS tuning_parameter_constraint (
 
 create index index_tuning_parameter_constraint on tuning_parameter_constraint (job_definition_id);
 
-alter table tuning_job_definition add column show_recommendation_count INT NOT NULL default 0;
+alter table tuning_job_definition add column show_recommendation_count INT default 0;
 
 # --- !Downs
 DELETE FROM tuning_parameter WHERE tuning_algorithm_id=3;
