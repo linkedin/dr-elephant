63e7725c773b1af0ac9efe484d9e4000e9578a10
ShubhamGupta29 <IIT2014061@iiita.ac.in>
2019-08-21 13:15:14 +0530

    Adding IDENTITY generation strategy for ID

--- app/models/JobsExceptionFingerPrinting.java
+++ app/models/JobsExceptionFingerPrinting.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import org.codehaus.jackson.annotate.JsonIgnore;
@@ -29,7 +30,7 @@ public class JobsExceptionFingerPrinting extends Model {
 
   @JsonIgnore
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   public int id;
 
   @Column(length = ID_LIMIT)
