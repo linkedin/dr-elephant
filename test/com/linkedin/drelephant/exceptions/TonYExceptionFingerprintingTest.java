4e61ab1d49d7fb0ce44412cf1b4b57b92f22d9a5
ShubhamGupta29 <IIT2014061@iiita.ac.in>
2020-02-11 01:59:31 +0530

    Moving out mock responses to resource files

--- test/com/linkedin/drelephant/exceptions/TonYExceptionFingerprintingTest.java
+++ test/com/linkedin/drelephant/exceptions/TonYExceptionFingerprintingTest.java
@@ -21,12 +21,15 @@ import com.linkedin.drelephant.analysis.AnalyticJob;
 import com.linkedin.drelephant.analysis.ApplicationType;
 import com.linkedin.drelephant.exceptions.util.ExceptionInfo;
 import com.linkedin.drelephant.exceptions.util.ExceptionUtils;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import models.AppResult;
+import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -72,83 +75,10 @@ public class TonYExceptionFingerprintingTest {
   private final String TEST_JOB_NAME_3 = "job_3";
   private final String TEST_AM_LOG_CONTAINER_URL_3 = "http://localhost:8042/node/containerlogs/container_e42_157609980900_38479_01_000001/user3";
 
-  private final String MOCK_RESPONSE_1 = new StringBuilder()
-      .append("<html>\n<body>\n<table>\n<pre>\n")
-      .append("20/01/24 21:28:46 INFO tony.ApplicationMaster: Unregistering task [worker:5] from Heartbeat monitor..\n")
-      .append("20/01/24 21:28:47 INFO tony.ApplicationMaster: Received result registration request with exit code 1 from worker 0\n")
-      .append("20/01/24 21:28:47 INFO tony.ApplicationMaster: Unregistering task [worker:0] from Heartbeat monitor..\n")
-      .append("20/01/24 21:28:47 INFO tony.ApplicationMaster: Completed containers: 2\n")
-      .append("20/01/24 21:28:47 INFO tony.ApplicationMaster: ContainerID = container_e42_1576097000949_30598_01_000013, state = COMPLETE, exitStatus = 1\n")
-      .append("20/01/24 21:28:47 ERROR tony.ApplicationMaster: [2020-01-24 21:28:46.848]Exception from container-launch.\n")
-      .append("Container id: container_e42_1576097000949_30598_01_000013\n")
-      .append("Exit code: 1\n").append("Shell output: main : command provided 1\n")
-      .append("main : run as user is user_1\n")
-      .append("main : requested yarn user is user_1\n")
-      .append("Getting exit code file...\n")
-      .append("Creating script paths...\n")
-      .append("Writing pid file...\n")
-      .append("Writing to tmp file /grid/c/tmp/yarn/nmPrivate/application_1576097000949_30598/container_e42_1576097000949_30598_01_000013/container_e42_1576097000949_30598_01_000013.pid.tmp\n")
-      .append("Writing to cgroup task files...\n")
-      .append("Creating local dirs...\n")
-      .append("Launching container...\n")
-      .append("Getting exit code file...\n")
-      .append("Creating script paths...\n") .append("\n").append("\n")
-      .append("[2020-01-24 21:28:46.869]Container exited with a non-zero exit code 1. Error file: prelaunch.err.\n")
-      .append("Last 4096 bytes of prelaunch.err :\n") .append("Last 4096 bytes of stderr :\n")
-      .append("20/01/24 21:28:45 WARN util.NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable\n")
-      .append("20/01/24 21:28:46 WARN shortcircuit.DomainSocketFactory: The short-circuit local reads feature cannot be used because libhadoop cannot be loaded.\n")
-      .append("Traceback (most recent call last):\n")
-      .append("Sample_traceback\n")
-      .append("tensorflow.python.framework.errors_impl.NotFoundError: hdfs://hostname.linkedin.com:9000/trainingData; No such file or directory\n\n")
-      .append("20/01/24 21:28:46 INFO util.Utils: pollTillNonNull function finished within 60 seconds\n")
-      .append("20/01/24 21:28:46 INFO tony.TaskExecutor: AM response for result execution run: RECEIVED\n")
-      .append("20/01/24 21:28:46 INFO tony.TaskExecutor: Child process exited with exit code 1\n")
-      .append("</pre>\n</table>\n</body>\n</html>\n").toString();
-
-  private final String MOCK_RESPONSE_2 = new StringBuilder()
-      .append("<html>\n<body>\n<table>\n<pre>\n")
-      .append("2020-02-05 02:56:18 INFO  ApplicationMaster:978 - ContainerID = container_e42_1576097000949_37909_01_000002, state = COMPLETE, exitStatus = 1\n")
-      .append("2020-02-05 02:56:18 ERROR ApplicationMaster:983 - [2020-02-05 02:56:18.039]Exception from container-launch.\n")
-      .append("Container id: container_e42_1576097000949_37909_01_000002\n")
-      .append("Exit code: 1\n")
-      .append("Shell output: main : command provided 1\n")
-      .append("main : run as user is sqf\n")
-      .append("main : requested yarn user is user2\n")
-      .append("Getting exit code file...\n")
-      .append("Creating script paths...\n")
-      .append("Writing pid file...\n")
-      .append("Writing to tmp file /grid/a/tmp/yarn/nmPrivate/application_1576097000949_37909/container_e42_1576097000949_37909_01_000002/container_e42_1576097000949_37909_01_000002.pid.tmp\n")
-      .append("Writing to cgroup task files...\n")
-      .append("Creating local dirs...\n")
-      .append("Launching container...\n")
-      .append("Getting exit code file...\n")
-      .append("Creating script paths...\n")
-      .append("\n")
-      .append("\n")
-      .append("[2020-02-05 02:56:18.081]Container exited with a non-zero exit code 1. Error file: prelaunch.err.\n")
-      .append("Last 4096 bytes of prelaunch.err :\n")
-      .append("Last 4096 bytes of stderr :\n")
-      .append("ib/python3.6/site-packages/tensorflow/python/estimator/estimator.py\", line 354, in train\n")
-      .append("    loss = self._train_model(input_fn, hooks, saving_listeners)\n")
-      .append("  File \"/grid/b/tmp/yarn/usercache/user2/appcache/application_1576097000949_37909/container_e42_1576097000949_37909_01_000002/venv/Python/lib/python3.6/site-packages/tensorflow/python/framework/errors_impl.py\", line 528, in __exit__\n")
-      .append("    c_api.TF_GetCode(self.status.status))\n")
-      .append("tensorflow.python.framework.errors_impl.InvalidArgumentError: Unsuccessful TensorSliceReader constructor: Failed to get matching files on hdfs://namenode.linkedin.com:9000/folder/GBert/bert-base/checkpoints/model.ckpt-1724730: Not found: hdfs://namenode.linkedin.com:9000/folder/GBert/bert-base/checkpoints; No such file or directory\n")
-      .append("\n")
-      .append("\n")
-      .append("[2020-02-05 02:56:18.081]Container exited with a non-zero exit code 1. Error file: prelaunch.err.\n")
-      .append("Last 4096 bytes of prelaunch.err :\n") .append("Last 4096 bytes of stderr :\n")
-      .append("ib/python3.6/site-packages/tensorflow/python/estimator/estimator.py\", line 354, in train\n")
-      .append("    loss = self._train_model(input_fn, hooks, saving_listeners)\n")
-      .append("  File \"/grid/b/tmp/yarn/usercache/user2/appcache/application_1576097000949_37909/container_e42_1576097000949_37909_01_000002/venv/Python/lib/python3.6/site-packages/tensorflow/python/estimator/estimator.py\", line 1207, in _train_model\n")
-      .append("    return self._train_model_default(input_fn, hooks, saving_listeners)\n")
-      .append("  File \"/grid/b/tmp/yarn/usercache/user2/appcache/application_1576097000949_37909/container_e42_1576097000949_37909_01_000002/venv/Python/lib/python3.6/site-packages/tensorflow/python/pywrap_tensorflow_internal.py\", line 326, in NewCheckpointReader\n")
-      .append("    return CheckpointReader(compat.as_bytes(filepattern), status)\n")
-      .append("  File \"/grid/b/tmp/yarn/usercache/user2/appcache/application_1576097000949_37909/container_e42_1576097000949_37909_01_000002/venv/Python/lib/python3.6/site-packages/tensorflow/python/framework/errors_impl.py\", line 528, in __exit__\n")
-      .append("    c_api.TF_GetCode(self.status.status))\n")
-      .append("tensorflow.python.framework.errors_impl.InvalidArgumentError: Unsuccessful TensorSliceReader constructor: Failed to get matching files on hdfs://namenode.linkedin.com:9000/folder/GBert/bert-base/checkpoints/model.ckpt-1724730: Not found: hdfs://namenode.linkedin.com:9000/folder/GBert/bert-base/checkpoints; No such file or directory\n")
-      .append("\n")
-      .append("\n")
-      .append("</pre>\n</table>\n</body>\n</html>\n").toString();
+  private final String FAKE_RESPONSE_APP_1_STDERR_PATH = "test/resources/exception/TonY/app_1_stderr_response.html";
+  private final String FAKE_RESPONSE_APP_1_STDOUT_PATH = "test/resources/exception/TonY/app_1_stdout_response.html";
+  private final String FAKE_RESPONSE_APP_2_STDERR_PATH = "test/resources/exception/TonY/app_2_stderr_response.html";
+  private final String FAKE_RESPONSE_APP_2_STDOUT_PATH = "test/resources/exception/TonY/app_2_stdout_response.html";
 
   @Before
   public void setup() {
@@ -218,9 +148,9 @@ public class TonYExceptionFingerprintingTest {
     running(testServer(TEST_SERVER_PORT, fakeApp), () -> {
       try {
         mockResponseForContainerLogs(new URL(TEST_AM_LOG_CONTAINER_URL_1).getPath() + stderrContainerLogParameters,
-            MOCK_RESPONSE_1, OK);
+            getFakeResponse(FAKE_RESPONSE_APP_1_STDERR_PATH), OK);
         mockResponseForContainerLogs(new URL(TEST_AM_LOG_CONTAINER_URL_1).getPath() + stdoutContainerLogParameters,
-            "", OK);
+            getFakeResponse(FAKE_RESPONSE_APP_1_STDOUT_PATH), OK);
       } catch (MalformedURLException ex) {
         logger.error("URL for test is not formed properly");
       }
@@ -230,7 +160,7 @@ public class TonYExceptionFingerprintingTest {
       TonYExceptionFingerprinting tonyEF = new TonYExceptionFingerprinting(fakeJob1, fakeAppResult1);
       tonyEF.doExceptionPrinting();
       List<ExceptionInfo> exceptionInfos = tonyEF.get_exceptionInfoList();
-      assertEquals(4, exceptionInfos.size());
+      assertEquals(5, exceptionInfos.size());
       assertEquals("Job Diagnostics", exceptionInfos.get(0).getExceptionName());
       assertEquals("Job Diagnostics: \n" + fakeJob1.getJobDiagnostics(), exceptionInfos.get(0).getExceptionStackTrace());
       assertEquals("Container exited with a non-zero exit code 1. Error file: prelaunch.err.", exceptionInfos.get(1).getExceptionName());
@@ -243,9 +173,9 @@ public class TonYExceptionFingerprintingTest {
     running(testServer(TEST_SERVER_PORT, fakeApp), () -> {
       try {
         mockResponseForContainerLogs(new URL(TEST_AM_LOG_CONTAINER_URL_2).getPath() + stderrContainerLogParameters,
-            "", OK);
+            getFakeResponse(FAKE_RESPONSE_APP_2_STDERR_PATH), OK);
         mockResponseForContainerLogs(new URL(TEST_AM_LOG_CONTAINER_URL_2).getPath() + stdoutContainerLogParameters,
-            MOCK_RESPONSE_2, OK);
+            getFakeResponse(FAKE_RESPONSE_APP_2_STDOUT_PATH), OK);
       } catch (MalformedURLException ex) {
         logger.error("URL for test is not formed properly");
       }
@@ -255,19 +185,18 @@ public class TonYExceptionFingerprintingTest {
       TonYExceptionFingerprinting tonyEF = new TonYExceptionFingerprinting(fakeJob, fakeAppResult);
       tonyEF.doExceptionPrinting();
       List<ExceptionInfo> exceptionInfos = tonyEF.get_exceptionInfoList();
-      assertEquals(4, exceptionInfos.size());
+      assertEquals(6, exceptionInfos.size());
       assertEquals("Job Diagnostics", exceptionInfos.get(0).getExceptionName());
       assertEquals("Job Diagnostics: \n" + fakeJob.getJobDiagnostics(), exceptionInfos.get(0)
           .getExceptionStackTrace());
       assertEquals("Container exited with a non-zero exit code 1. Error file: prelaunch.err.",
           exceptionInfos.get(1).getExceptionName());
-      assertEquals("ERROR ApplicationMaster:983 - [2020-02-05 02:56:18.039]Exception from container-launch.",
-          exceptionInfos.get(3).getExceptionName());
+      assertEquals("ERROR ApplicationMaster:983 - [2020-02-05 02:56:19.427]Container killed by the ApplicationMaster.",
+          exceptionInfos.get(5).getExceptionName());
       assertEquals(ExceptionUtils.ConfigurationBuilder.NUMBER_OF_STACKTRACE_LINE.getValue() + 1,
-          (exceptionInfos.get(3).getExceptionStackTrace().split("\n")).length);
+          (exceptionInfos.get(5).getExceptionStackTrace().split("\n")).length);
     });
   }
-
 
   @Test
   public void testTonyExceptionFingerprintingWhenNoLogFound()
@@ -292,5 +221,15 @@ public class TonYExceptionFingerprintingTest {
       assertEquals("Job Diagnostics: \n" + fakeJob.getJobDiagnostics(), exceptionInfos.get(0)
           .getExceptionStackTrace());
     });
+  }
+
+  private String getFakeResponse(String path) {
+    try {
+      FileInputStream inputStream = new FileInputStream(path);
+      return IOUtils.toString(inputStream);
+    } catch (IOException ex) {
+      logger.error("Exception while parsing fake response");
+      return "";
+    }
   }
 }
