25ff778339ecbe72c43325f17bf3109180fd4e06
saniyanajeeb <saniyanajeeb22@gmail.com>
2020-04-17 17:39:26 +0530

    fix in build.sh to take values from env variables

--- build.sh
+++ build.sh
@@ -6,8 +6,14 @@ rm -rf "$BUILD_PATH"/app-conf/elephant.conf.bak
 
 cd $BUILD_PATH/
 
-# Compile the Dr Elephant code
-bash ./compile.sh
+
+# Compile Dr Elephant code with the script provided. An optional conf file can be passed in.
+if [ -z "$1" ];
+then
+  bash ./compile.sh
+else
+  bash ./compile.sh $1
+fi
 
 # Generate the pom file (other artifacts are disabled) and publish to bintray
 sbt clean 'set publishArtifact in (Compile, packageBin) := false' 'set publishArtifact in (Compile, packageDoc) := false' 'set publishArtifact in (Compile, packageSrc) := false' publish
@@ -21,7 +27,7 @@ mv $BUILD_PATH/dist/dr-elephant*.zip $BUILD_PATH/dist/$MODULE-$VERSION.zip
 cd dist/
 
 echo "Uploading the distribution to bintray"
-curl -T $MODULE-"$VERSION".zip -u"$1":"$2" https://api.bintray.com/content/linkedin/maven/dr-elephant/"$VERSION"/com/linkedin/drelephant/$MODULE/"$VERSION"/ --verbose
+curl -T $MODULE-"$VERSION".zip -u"$BINTRAY_USER":"$BINTRAY_PASS" https://api.bintray.com/content/linkedin/maven/dr-elephant/"$VERSION"/com/linkedin/drelephant/$MODULE/"$VERSION"/ --verbose
 
 echo "Publishing the uploaded distribution"
-curl -X POST -u"$1":"$2" https://api.bintray.com/content/linkedin/maven/dr-elephant/"$VERSION"/publish --verbose
+curl -X POST -u"$BINTRAY_USER":"$BINTRAY_PASS" https://api.bintray.com/content/linkedin/maven/dr-elephant/"$VERSION"/publish --verbose
